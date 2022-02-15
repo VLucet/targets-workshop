b4bf783477a42ea4a372951f7f6b5ac6ae766e38
Valentin <valentin.lucet@gmail.com>
2022-02-15 16:19:48 -0500

    fix, basic file saving workflow

---
+++ R/functions.R
@@ -0,0 +1,10 @@
+
+# R functions for pipeline ------------------------------------------------
+
+# Get the palmer penguins data and save it as a file
+
+get_palmer_data <- function(path = "data/penguins.csv"){
+  penguins_data <- palmerpenguins::penguins
+  write.csv(penguins_data, path)
+  return(path)
+}
---
+++ _targets.R
@@ -0,0 +1,21 @@
+library(targets)
+source("R/functions.R")
+list(
+  tar_target(
+    download_data,
+    get_palmer_data(),
+    format = "file"
+  )#,
+  # tar_target(
+  #   raw_data,
+  #   read_csv(raw_data_file, col_types = cols())
+  # ),
+  # tar_target(
+  #   data,
+  #   raw_data %>%
+  #     filter(!is.na(Ozone))
+  # ),
+  # tar_target(hist, create_plot(data)),
+  # tar_target(fit, biglm(Ozone ~ Wind + Temp, data)),
+  # tar_render(report, "index.Rmd")
+)
